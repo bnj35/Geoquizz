1cd74cd5b0e34b2d85ff93ec068c07b7f930f945
VadimElFamoso <vadimprofessionel@gmail.com>
2025-02-04 11:34:06 +0100

    feat: GameView mvp

---
+++ frontend/src/components/GameConfirmComponent.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="absolute bg-green-400 px-4 py-2 left-1/2 -translate-x-1/2" id="ConfirmButton">
+    <button class="">Confirmer votre choix</button>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
