9bb16723c8db2aec6e062aef79b83ef08e678fa1
MaximeBiechy <maxime.biechy5@etu.univ-lorraine.fr>
2025-02-05 17:56:17 +0100

    ajout plugin axios avec bugs

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,14 +6,14 @@ import piniaPersist from 'pinia-plugin-persist';
 
 import App from './App.vue'
 import router from './router'
-import api from "./utils/api.js";
+import api from "./utils/api";
 
 const app = createApp(App)
 
 const pinia = createPinia();
 pinia.use(piniaPersist)
 app.use(pinia)
-app.use(api, {baseUrl: import.meta.env.VITE_API_URL})
+app.use(api, {baseUrl: import.meta.env.VITE_API_BASE_URL})
 app.use(router)
 
 app.mount('#app')
