eaea978b8ea8683d5cc3e4abd86c2c3c4ce06cdf
Timothée Benchergui <bencherg2u@etu.univ-lorraine.fr>
2025-02-07 10:40:07 +0100

    middleware d'authentification

--- backend/gateway/config/routes.php
+++ backend/gateway/config/routes.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 use geoquizz\application\actions\GatewayAuthAction;
 use geoquizz\application\actions\GatewayDirectusAction;
+use geoquizz\application\middlewares\Auth;
 use Slim\App;
 use geoquizz\application\actions\GatewayPlayerAction;
 use geoquizz\application\actions\GatewayAssetsAction;
@@ -23,13 +24,15 @@ return function(App $app):App {
     $app->post('/stats[/]', GatewayPlayerAction::class);
     $app->put('/stats/{id}[/]', GatewayPlayerAction::class);
 
-    $app->post('/parties[/]', GatewayPlayerAction::class)->setName('createPartie');
+    $app->post('/parties[/]', GatewayPlayerAction::class)->setName('createPartie')
+        ->add(Auth::class);
 
     $app->get('/parties[/]', GatewayPlayerAction::class)->setName('getParties');
 
     $app->get('/parties/{id}[/]', GatewayPlayerAction::class)->setName('getPartieById');
 
-    $app->get('/users/{id}/parties[/]', GatewayPlayerAction::class)->setName('getPartiesByUser');
+    $app->get('/users/{id}/parties[/]', GatewayPlayerAction::class)->setName('getPartiesByUser')
+        ->add(Auth::class);
 
     $app->patch('/parties/{id}/score[/]', GatewayPlayerAction::class)->setName('updateScore');
 
@@ -41,7 +44,8 @@ return function(App $app):App {
     $app->post('/validate[/]', GatewayAuthAction::class)->setName('validate');
     $app->get('/users[/]', GatewayAuthAction::class)->setName('users');
     $app->get('/users/{id}[/]', GatewayAuthAction::class)->setName('user');
-    $app->get('/users/{id}/stats[/]', GatewayPlayerAction::class)->setName('getStatsByUser');
+    $app->get('/users/{id}/stats[/]', GatewayPlayerAction::class)->setName('getStatsByUser')
+        ->add(Auth::class);
 
     $app->get('/assets/{id}',GatewayAssetsAction::class)->setName('getAssets');
     $app->get('/items/series', GatewayDirectusAction::class)->setName('getSeries');
